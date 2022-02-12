44ceb3726ef4fefc1c9c2180764e6464c3612e6c
s1mple-person <sergey.tkch25@gmail.com>
2022-02-12 11:08:59 +0300

    typeof

--- min.max.js
+++ min.max.js
@@ -1,7 +1,7 @@
 let a ="10"
 let b ="15"
 function min(a, b) {
-    if (a > b) {
+    if (a < b) {
       return a;
     } else {
       return b;
---
+++ typeof.js
@@ -0,0 +1,4 @@
+console.log(typeof 33);
+console.log(typeof 'macan');
+console.log(typeof true);
+console.log(typeof vargh)
