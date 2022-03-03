50f0314323c51f48577afcc821a723c7751ad42e
s1mple-person <sergey.tkch25@gmail.com>
2022-03-03 09:57:22 +0300

    создаем маршрутизатор

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require("http");
 const requestHandler = (request, response) => {
-console.log("Url: " + request.url);
-console.log("Тип запроса: " + request.method);
-console.log("User-Agent: " + request.headers["user-agent"]);
-console.log("Все заголовки");
-console.log(request.headers);
-response.end("rabotaet nakonecto");
+    console.log("Url: " + request.url);
+    console.log("Тип запроса: " + request.method);
+    console.log("User-Agent: " + request.headers["user-agent"]);
+    console.log("Все заголовки");
+    console.log(request.headers);
+    response.end("rabotaet nakonecto");
 };
 http.createServer(requestHandler).listen(3000);
