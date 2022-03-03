0dc2acb6f3083432e85a3aa50e0fec437ac1fde7
s1mple-person <sergey.tkch25@gmail.com>
2022-03-03 16:16:52 +0300

    логирование

--- logirovanie.js
+++ logirovanie.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
 const http = require("http");
 const requestHandler = (request, response) => {
-response.setHeader("Content-Type", "text/html; charset=utf-8;");
-if (request.url === "/home" || request.url === "/") {
-response.write("<h2>Home</h2>");
-} else if (request.url == "/about") {
-response.write("<h2>About</h2>");
-} else if (request.url == "/contact") {
-response.write("<h2>Contacts</h2>");
-} else {
-response.write("<h2>Not found</h2>");
-}
-response.end();
-console.log("Url: " + request.url);
-console.log("Тип запроса: " + request.method);
-console.log("User-Agent: " + request.headers["user-agent"]);
-console.log("Все заголовки");
-console.log(request.headers);
-let user = JSON.stringify(request.headers);
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    if (request.url === "/home" || request.url === "/") {
+        response.write("<h2>Home</h2>");
+    } else if (request.url == "/about") {
+        response.write("<h2>About</h2>");
+    } else if (request.url == "/contact") {
+        response.write("<h2>Contacts</h2>");
+    } else {
+        response.write("<h2>Not found</h2>");
+    }
+    response.end();
+    console.log("Url: " + request.url);
+    console.log("Тип запроса: " + request.method);
+    console.log("User-Agent: " + request.headers["user-agent"]);
+    console.log("Все заголовки");
+    console.log(request.headers);
+    let user = JSON.stringify(request.headers);
 
-fs.appendFileSync("привет.txt", request.url);
-fs.appendFileSync("привет.txt", request.method);
-fs.appendFileSync("привет.txt", user);
+    fs.appendFileSync("привет.txt", request.url);
+    fs.appendFileSync("привет.txt", request.method);
+    fs.appendFileSync("привет.txt", user);
 
 };
 http.createServer(requestHandler).listen(3000);
